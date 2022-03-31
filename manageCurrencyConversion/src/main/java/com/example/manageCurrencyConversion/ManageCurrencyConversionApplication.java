96f9ba96f999090876939ff2617ff00e36082dd2
Deepa Anchan <deepa.anchan97@gmail.com>
2022-03-31 16:48:59 +0530

    Kubernates doesnot need zipkin, rabbitmq

--- manageCurrencyConversion/src/main/java/com/example/manageCurrencyConversion/ManageCurrencyConversionApplication.java
+++ manageCurrencyConversion/src/main/java/com/example/manageCurrencyConversion/ManageCurrencyConversionApplication.java
@@ -2,21 +2,13 @@ package com.example.manageCurrencyConversion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 
-import brave.sampler.Sampler;
 
-@SpringBootApplication
-@EnableDiscoveryClient  
+@SpringBootApplication 
 public class ManageCurrencyConversionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManageCurrencyConversionApplication.class, args);
 	}
 
-//	@Bean
-//	public Sampler defaultSampler() {
-//		return Sampler.ALWAYS_SAMPLE;
-//	}
 }
